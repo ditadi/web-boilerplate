8a10343036cc8761031ef9dc7f8fa597940fa5f7
VICTOR DITADI <victor.ditadi@casafari.com>
2023-05-15 11:40:29 +0100

    build: Storybook Repo

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
+	extends: ["next", "turbo", "prettier", "plugin:storybook/recommended"],
+	rules: {
+		"@next/next/no-html-link-for-pages": "off",
+	},
+	parserOptions: {
+		babelOptions: {
+			presets: [require.resolve("next/babel")],
+		},
+	},
 };
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,7 +1,22 @@
-"use client";
+interface Props {
+	primary?: boolean;
+	size?: "small" | "large";
+	label?: string;
+}
 
-import * as React from "react";
-
-export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop</button>;
+export const Button = ({
+	primary = false,
+	label = "Boop",
+	size = "small",
+}: Props) => {
+	return (
+		<button
+			style={{
+				backgroundColor: primary ? "red" : "blue",
+				fontSize: size === "large" ? "24px" : "14px",
+			}}
+		>
+			{label}
+		</button>
+	);
 };
