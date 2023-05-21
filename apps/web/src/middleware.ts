f83e2a861085b786ebfbde7da811c21c69164f7f
VICTOR DITADI <victor.ditadi@casafari.com>
2023-05-21 17:11:48 +0100

    fixing

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
