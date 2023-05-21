2e008d595a81d0596edaa79bf99b35a2aab03e12
VICTOR DITADI <victor.ditadi@casafari.com>
2023-05-21 17:05:10 +0100

    fixing

--- apps/web/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from '@clerk/nextjs';
-
-export default authMiddleware({
-  debug: true,
-});
-
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
