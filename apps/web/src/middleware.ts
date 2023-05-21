5a99d00d432285072edb18b0b1da6e577affb03f
VICTOR DITADI <victor.ditadi@casafari.com>
2023-05-21 15:14:22 +0100

    fix(clerk and middleware integration): fix clerk and next middleware integration

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,11 +1,18 @@
 import { authMiddleware } from '@clerk/nextjs';
+import { NextRequest, NextResponse } from 'next/server';
 
-const publicRoutes = ["/sign-in", "/sign-up"]
+const publicRoutes = ['/sign-in', '/sign-up'];
 
 export default authMiddleware({
-	publicRoutes
+  beforeAuth: (req: NextRequest) => {
+    if (req.nextUrl.pathname === '/') {
+      const dashboardURL = new URL('/dashboard', req.url);
+      return NextResponse.redirect(dashboardURL);
+    }
+  },
+  publicRoutes,
 });
 
 export const config = {
-	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
