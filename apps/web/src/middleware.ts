d47183c9260793f2f31d0123e0d0ffc1b111b4d5
VICTOR DITADI <victor.ditadi@casafari.com>
2023-05-22 15:19:28 +0100

    fixing

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,7 +1,14 @@
 import { authMiddleware } from '@clerk/nextjs';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default authMiddleware();
-
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+export default authMiddleware({
+	beforeAuth: (req: NextRequest) => {
+		if (req.nextUrl.pathname === '/') {
+			const dashboardURL = new URL('/dashboard', req.url);
+			return NextResponse.redirect(dashboardURL);
+		} else {
+			return NextResponse.next();
+		}
+	},
+	debug: true,
+});
